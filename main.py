d1e4dc5635b19ce317eea957325cb4970da9af1c
gabriel-rosa-tech <61751895+gabriel-rosa-tech@users.noreply.github.com>
2024-01-29 19:08:49 -0300

    Update main.py

--- main.py
+++ main.py
@@ -346,12 +346,14 @@ dic_group_keys = {
 
 def group_sum(df, periodo):
     st.write(dic_group_keys[periodo])
-    df_fob_grouped = df['preco'].groupby(dic_group_keys[periodo]).sum()
+    selecao = ['preco', dic_group_keys[periodo]]
+    df_fob_grouped = df[selecao].groupby(dic_group_keys[periodo]).sum()
     df_fob_grouped['x'] = df_fob_grouped.index 
     return df_fob_grouped
 
 def group_mean(df, periodo):
-    df_fob_grouped = df['preco'].groupby(dic_group_keys[periodo]).mean()
+    selecao = ['preco', dic_group_keys[periodo]]
+    df_fob_grouped = df[selecao].groupby(dic_group_keys[periodo]).mean()
     df_fob_grouped['x'] = df_fob_grouped.index 
     return df_fob_grouped
 
